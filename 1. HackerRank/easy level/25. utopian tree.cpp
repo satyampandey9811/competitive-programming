6ff4474d958fbf56771f3258f2f7d45d071ccb74
Satyam Pandey <satyampandey9811@gmail.com>
2019-09-25 00:18:38 +0530

    utopian tree

---
+++ 1. HackerRank/easy level/25. utopian tree.cpp
@@ -0,0 +1,44 @@
+// link to question - https://www.hackerrank.com/challenges/utopian-tree/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the utopianTree function below.
+int utopianTree(int n) {
+    int a=1;
+    if(n==0)
+        return 1;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            a*=2;
+        }
+        else if(i%2==0){
+            a+=1;
+        }
+    }
+    return a;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = utopianTree(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
