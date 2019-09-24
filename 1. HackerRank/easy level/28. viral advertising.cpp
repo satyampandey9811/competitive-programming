60c75b0a25be1dcc7c5ccea36e4bd82ff38d3457
Satyam Pandey <satyampandey9811@gmail.com>
2019-09-25 00:21:24 +0530

    viral advertising

---
+++ 1. HackerRank/easy level/28. viral advertising.cpp
@@ -0,0 +1,34 @@
+// link to question - https://www.hackerrank.com/challenges/strange-advertising/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the viralAdvertising function below.
+int viralAdvertising(int n) {
+
+    int s=5,l=0,c=0;
+    for( int i=1;i<=n;i++){
+        l=s/2;
+        c+=l;
+        s=l*3;
+    }
+    return c;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
