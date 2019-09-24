604c2b00e72ee0b41e3a38ae3ec0bc0716ce1efa
Satyam Pandey <satyampandey9811@gmail.com>
2019-09-24 18:24:02 +0530

    counting valleys

---
+++ 1. HackerRank/easy level/19. counting valleys.cpp
@@ -0,0 +1,41 @@
+// link to question - https://www.hackerrank.com/challenges/counting-valleys/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the countingValleys function below.
+int countingValleys(int n, string s) {
+
+    int sl=0,b=0;
+    for (int i=0;i<n;i++){
+        if(sl==0 and s[i]=='D'){
+            b++;sl--;
+        }
+        else if(s[i]=='U')
+            sl++;
+        else if(s[i]=='D')
+            sl--;
+    }
+    return b;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int result = countingValleys(n, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
