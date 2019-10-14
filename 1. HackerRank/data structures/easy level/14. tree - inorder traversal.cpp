56bce38c30ee5a8e450d35d4f4f4e9534f8558fc
Satyam Pandey <satyampandey9811@gmail.com>
2019-10-14 21:55:35 +0530

    tree - inorder traversal

---
+++ 1. HackerRank/data structures/easy level/14. tree - inorder traversal.cpp
@@ -0,0 +1,28 @@
+// link to question - https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root) {
+        if(root==NULL) return;        
+        inOrder(root->left);
+        cout<<root->data<<' ';
+        inOrder(root->right);
+    }
+
