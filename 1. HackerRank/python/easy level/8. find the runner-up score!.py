541a534c0bd5235ad433cd3ac8c77e7f19c9d001
Satyam Pandey <satyampandey9811@gmail.com>
2019-10-29 12:12:01 +0530

    find the runner-up score!

---
+++ 1. HackerRank/python/easy level/8. find the runner-up score!.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+
+a = max(arr)
+while(a in arr):
+    arr.remove(a)
+a = max(arr)
+print(a)
+
+
