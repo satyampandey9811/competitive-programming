d944a3f7f1fb57ff1c97b22314b3d70b64d63bef
Satyam Pandey <satyampandey9811@gmail.com>
2019-11-01 00:18:16 +0530

    finding the percentage

---
+++ 1. HackerRank/python/easy level/10. finding the percentage.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+mylist = student_marks[query_name]
+sum = 0
+for i in mylist:
+    sum += i
+sum /= 3
+print("{:1.2f}".format(sum))
+    
