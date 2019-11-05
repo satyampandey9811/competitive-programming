82fb76546b99d2c419ca5d6163f725b664a33429
Satyam Pandey <satyampandey9811@gmail.com>
2019-11-05 22:05:58 +0530

    string split and join

---
+++ 1. HackerRank/python/easy level/14. string split and join.py
@@ -0,0 +1,7 @@
+# link to question - https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
