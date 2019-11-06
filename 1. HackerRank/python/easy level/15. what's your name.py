7a618158706626118ae9238f981f1533c24871ee
Satyam Pandey <satyampandey9811@gmail.com>
2019-11-06 18:02:07 +0530

    what's your name

---
+++ 1. HackerRank/python/easy level/15. what's your name.py
@@ -0,0 +1,5 @@
+# link to question - https://www.hackerrank.com/challenges/whats-your-name/problem
+
+def print_full_name(a, b):
+    print("Hello {} {}! You just delved into python.".format(a,b))
+
