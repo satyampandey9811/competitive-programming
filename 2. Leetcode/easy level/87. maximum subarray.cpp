04dbdffad93da8d9497bff01744e0d6b95c0f7e0
Satyam Pandey <satyampandey9811@gmail.com>
2020-01-17 20:54:54 +0530

    maximum subarray

---
+++ 2. Leetcode/easy level/87. maximum subarray.cpp
@@ -0,0 +1,15 @@
+// link to question - https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=INT_MIN,sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            ans=max(ans,sum);
+            if(sum<0)
+                sum=0;
+        }
+        return ans;
+    }
+};
