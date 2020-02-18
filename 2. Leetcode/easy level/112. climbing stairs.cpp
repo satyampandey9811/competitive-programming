1364c60ef2b04db288f708f2269d16ba1a057f19
Satyam Pandey <satyampandey9811@gmail.com>
2020-02-18 17:02:18 +0530

    climbing stairs

---
+++ 2. Leetcode/easy level/112. climbing stairs.cpp
@@ -0,0 +1,12 @@
+// link to question - https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+};
