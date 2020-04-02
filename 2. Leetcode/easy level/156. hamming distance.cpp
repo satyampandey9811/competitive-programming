968de9cf0119ca46b3254c2e77fe422b5bb31f9b
Satyam Pandey <satyampandey9811@gmail.com>
2020-04-02 16:02:03 +0530

    hamming distance

---
+++ 2. Leetcode/easy level/156. hamming distance.cpp
@@ -0,0 +1,14 @@
+// link to question - https://leetcode.com/problems/hamming-distance/
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;        
+        x = x ^ y;
+        while(x){
+            x = x & (x - 1);
+            ans++;
+        }
+        return ans;
+    }
+};
