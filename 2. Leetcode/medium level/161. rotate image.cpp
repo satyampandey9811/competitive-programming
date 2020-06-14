12501e0f5a1d9ccc46d0fe660ce3fa28641ff01f
Satyam Pandey <satyampandey9811@gmail.com>
2020-06-14 23:19:28 +0530

    rotate image

---
+++ 2. Leetcode/medium level/161. rotate image.cpp
@@ -0,0 +1,17 @@
+// link to question - https://leetcode.com/problems/rotate-image/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& a) {
+        int n = a.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                swap(a[i][j], a[j][i]);
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            reverse(a[i].begin(), a[i].end());
+        }
+    }
+};
