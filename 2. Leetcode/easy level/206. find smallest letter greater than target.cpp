fd59dfac661110cc1f3cdc85661b7184ae2fd10e
Satyam Pandey <satyampandey9811@gmail.com>
2020-06-22 23:55:24 +0530

    find smallest letter greater than target

---
+++ 2. Leetcode/easy level/206. find smallest letter greater than target.cpp
@@ -0,0 +1,10 @@
+// link to question - https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        
+         auto it = upper_bound(letters.begin(), letters.end(), target);
+        return it == letters.end() ? letters[0] : *it;
+    }
+};
