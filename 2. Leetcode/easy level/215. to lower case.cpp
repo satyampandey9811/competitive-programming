72508845b5850de7f4cd087172845dc93c42b2cb
Satyam Pandey <satyampandey9811@gmail.com>
2020-06-23 15:00:23 +0530

    to lower case

---
+++ 2. Leetcode/easy level/215. to lower case.cpp
@@ -0,0 +1,12 @@
+// link to question - https://leetcode.com/problems/to-lower-case/
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(int i=0;i<str.size();i++){
+            if(isupper(str[i]))
+            str[i]=tolower(str[i]);
+        }
+        return str;
+    }
+};
