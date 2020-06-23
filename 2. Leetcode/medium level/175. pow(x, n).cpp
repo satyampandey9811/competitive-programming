2dd1291446fd3ff5da0ba3a16f8d2b918bcad2f9
Satyam Pandey <satyampandey9811@gmail.com>
2020-06-23 15:07:04 +0530

    pow(x, n)

---
+++ 2. Leetcode/medium level/175. pow(x, n).cpp
@@ -0,0 +1,9 @@
+// link to question - https://leetcode.com/problems/powx-n/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double a=pow(x,n);
+        return a;
+    }
+};
