6ba9dab5e97b1bfc610db78ca26f73e3d93f9f93
Satyam Pandey <satyampandey9811@gmail.com>
2020-06-27 22:10:19 +0530

    the kth factor of n

---
+++ 2. Leetcode/medium level/187. the kth factor of n.cpp
@@ -0,0 +1,13 @@
+// link to question - https://leetcode.com/problems/the-kth-factor-of-n/
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int c = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) c++;
+            if(c == k) return i;
+        }
+        return -1;
+    }
+};
