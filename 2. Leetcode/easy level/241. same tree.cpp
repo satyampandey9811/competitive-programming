e613c1f7f9b9aaaf59e4a7d00f96ef4629950d82
Satyam Pandey <satyampandey9811@gmail.com>
2020-07-13 13:48:46 +0530

    same tree

---
+++ 2. Leetcode/easy level/241. same tree.cpp
@@ -0,0 +1,21 @@
+// link to question - https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p and !q) return 1;
+        if(!p or !q) return 0;
+        return p->val == q->val and isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+    }
+};
