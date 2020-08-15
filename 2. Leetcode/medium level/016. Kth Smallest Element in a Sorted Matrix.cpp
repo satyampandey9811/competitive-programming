c8891e09287ba7c9871615fad7e894890ee2e58a
Satyam Pandey <satyampandey9811@gmail.com>
2020-08-15 22:27:21 +0530

    kth smallest element in a sorted matrix

--- 2. Leetcode/medium level/016. Kth Smallest Element in a Sorted Matrix.cpp
+++ 2. Leetcode/medium level/016. Kth Smallest Element in a Sorted Matrix.cpp
@@ -3,16 +3,27 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& a, int k) {
-        vector<int> ans;
-        for(int i = 0; i < a.size(); i++){
-            for(int j = 0; j < a[0].size(); j++)
-                ans.push_back(a[i][j]);
+        int n = a.size();
+        int low = a[0][0], high = a[n-1][n-1], mid;
+        
+        while(low < high) {
+            mid = low + (high - low) / 2;
+            int c = 0;
+            
+            for(int i = 0; i < n; i++) {
+                int l = 0, r = n - 1, m, ans = -1;
+                while(l <= r) {
+                    m = l + (r - l) / 2;
+                    if(a[i][m] <= mid) ans = m, l = m + 1;  
+                    else r = m - 1;
+                }
+                c += ans + 1;
+            }
+            
+            if(c < k) low = mid + 1;
+            else high = mid;
         }
-        sort(ans.begin(),ans.end());
-        int c = 1;
-        while(c != k){
-            c++;
-        }  
-        return ans[c - 1];
+        
+        return low;
     }
 };
