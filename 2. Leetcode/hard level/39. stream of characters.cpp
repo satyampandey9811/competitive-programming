0964b22cd7fb16ce3ba062d4f3bf174c4dcd0f74
Satyam Pandey <satyampandey9811@gmail.com>
2020-08-23 23:06:25 +0530

    stream of characters updated

--- 2. Leetcode/hard level/39. stream of characters.cpp
+++ 2. Leetcode/hard level/39. stream of characters.cpp
@@ -4,37 +4,69 @@ class trienode {
 public:
     char data;
     bool isterminal;
-    vector<trienode*> children(26);
+    vector<trienode*> children;
     
     trienode(char data) {
         this -> data = data;
         isterminal = false;
-        for(int i = 0; i < 26; i++) children[i] = NULL;
+        children.resize(26, NULL);        
     }
-}
+};
 
 class StreamChecker {
 public:
     trienode *root = new trienode('\0');
+    string s;
     
-    void insert(trienode* root, string &word, int i) {
-        if(i == word.size()) {            
-            root -> isterminal = true;
-            return;
-        }
-        int index = word[i] - 'a';
-        trienode *child = root -> children[index];
-        if(!child) child = new trienode(word[i]);                
-        insert(child, word, i + 1);
-    }
+    // void insert(trienode* root, string &word, int i) {
+    //     if(i == word.size()) {            
+    //         root -> isterminal = true;
+    //         return;
+    //     }
+    //     int index = word[i] - 'a';
+    //     trienode *child = root -> children[index];
+    //     if(!child) {
+    //         child = new trienode(word[i]);                
+    //         root -> children[index] = child;
+    //     }
+    //     insert(child, word, i + 1);
+    // }
     
     StreamChecker(vector<string>& words) {
-        for(int i = 0; i < words.size(); i++) 
-            insert(root, words[i], 0);
+        for(int i = 0; i < words.size(); i++) {
+            string word = words[i];
+            reverse(word.begin(), word.end());
+            trienode *temp = root;
+            for(char c: word) {
+                int index = c - 'a';
+                if(temp -> children[index] == NULL) {
+                    temp -> children[index] = new trienode(c);
+                }
+                temp = temp -> children[index];                
+            }
+            temp -> isterminal = true;
+        }
     }
     
+    // bool search(trienode* root, string &word, int i) {
+    //     if(i == word.size()) {
+    //         return root -> isterminal;
+    //     }
+    //     int index = word[i] - 'a';
+    //     if(root -> children[index] == NULL) return 0;
+    //     return search(root -> children[index], word, i + 1);
+    // }
+    
     bool query(char letter) {
-        
+        s += letter;        
+        trienode *temp = root;
+        for(int i = s.size() - 1; i >= 0; i--) {                    
+            int index = s[i] - 'a';
+            temp = temp -> children[index];
+            if(temp == NULL) return 0;
+            if(temp -> isterminal) return 1;
+        }
+        return 0;
     }
 };
 
