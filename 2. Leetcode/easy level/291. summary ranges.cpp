f6c70ae509f9a3f4679bdf5b62bed46c6bc85255
Satyam Pandey <satyampandey9811@gmail.com>
2020-10-28 13:49:50 +0530

    summary ranges

---
+++ 2. Leetcode/easy level/291. summary ranges.cpp
@@ -0,0 +1,24 @@
+// link to question - https://leetcode.com/problems/summary-ranges/
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& a) {
+        int n = a.size();
+        vector<string> ans;
+        if(n == 0) return ans;
+        
+        int s = a[0], e = a[0];
+        for(int i = 1; i < n; i++) {
+            if(a[i] != e + 1) {
+                if(s == e) ans.push_back(to_string(s));
+                else ans.push_back(to_string(s) + "->" + to_string(e));
+                s = e = a[i];
+            }
+            else e = a[i];
+        }
+        if(s == e) ans.push_back(to_string(s));
+        else ans.push_back(to_string(s) + "->" + to_string(e));
+        
+        return ans;
+    }
+};
