a1822e09fe7347f45b9d12d3356833370d907bf2
Satyam Pandey <satyampandey9811@gmail.com>
2022-08-07 12:17:57 +0530

    number of arithmetic triplets

---
+++ 2. Leetcode/easy level/427. number of arithmetic triplets.cpp
@@ -0,0 +1,16 @@
+// link to question - https://leetcode.com/problems/number-of-arithmetic-triplets/
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& a, int d) {
+        int n = a.size(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (a[j] - a[i] == d and a[k] - a[j] == d) ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
