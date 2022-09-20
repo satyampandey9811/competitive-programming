fb50f13cbb9fc9370455ede741516aac3342b41e
Satyam Pandey <satyampandey9811@gmail.com>
2022-09-20 23:51:28 +0530

    smallest even multiple

---
+++ 2. Leetcode/easy level/433. smallest even multiple.cpp
@@ -0,0 +1,9 @@
+// link to question - https://leetcode.com/problems/smallest-even-multiple/
+
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) return n;
+        return n * 2;
+    }
+};
